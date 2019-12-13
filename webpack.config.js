934b1ec2bdacade987dc2ac9832d0be82165b782
reifujimura <25904196+reifujimura@users.noreply.github.com>
2019-12-13 20:48:20 +0900

    Update config

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,12 @@ module.exports = [
         {
           test: /\.(ts|tsx)$/,
           exclude: /node_modules/,
-          loader: "ts-loader"
+          loader: "ts-loader",
+          options: {
+            compilerOptions: {
+              sourceMap: process.env.NODE_ENV === "development"
+            }
+          }
         }
       ]
     },
